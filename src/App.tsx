188cbc1ed6cf9547d1e2da1f5ac46ba7f920acff
Conor Dunne <conordunne@outlook.ie>
2023-08-14 11:53:38 +0900

    Create Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,64 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import Header from "./assets/Header";
+import "./App.css";
 
 interface Character {
   literal: string;
   readingMeaning: {
-    groups: [{
-      meanings: { lang: string; value: string }[];
-      readings: {value: string, type: string}[];
-    }];
-
+    groups: [
+      {
+        meanings: { lang: string; value: string }[];
+        readings: { value: string; type: string }[];
+      }
+    ];
   };
-  misc: {jlptLevel: number; grade: number};
+  misc: { jlptLevel: number; grade: number };
 }
 
 function App() {
   const [importedData, setImportedData] = useState<Character[]>([]);
 
   useEffect(() => {
-    fetch('/kanjidic2-en-3.5.0.json?url')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
+    fetch("/kanjidic2-en-3.5.0.json?url")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         if (data && data.characters) {
           setImportedData(data.characters);
         }
       })
-      .catch(error => {
-        console.error('Error fetching JSON data:', error);
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
       });
   }, []);
   return (
-    <div className='kanji-wrapper'>
+    <>
+    <Header />
+    <div className="kanji-wrapper">
       {importedData.map((character: Character, index: number) => {
-        if(character.misc.grade === 1) {
+        if (character.misc.grade === 1) {
           return (
-            <div key={index}>
-              <p>{character.literal}</p>
-              <p>{character.readingMeaning.groups[0].readings.map(reading => reading.type === "ja_on" ? reading.value : null) }</p>
-              <p>{character.readingMeaning.groups[0].meanings.map(meaning => meaning.value).join(', ')}</p>
+            <div key={index} className="kanji-card">
+              <p className="literal">{character.literal}</p>
+              <p>
+                {character.readingMeaning.groups[0].readings
+                  .flatMap((reading) =>
+                    reading.type === "ja_on" ? [reading.value] : []
+                  )
+                  .join(", ")}
+              </p>
+              <p>
+                {character.readingMeaning.groups[0].meanings
+                  .map((meaning) => meaning.value)
+                  .join(", ")}
+              </p>
             </div>
-          )
-        } 
+          );
+        }
       })}
     </div>
+    </>
   );
 }
 
 export default App;
-
-
-
-
